24b1c4b1a5c8859d5ce9accf0365a437faa01d42
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-09-15 15:38:26 -0400

    Add Elasticsearch for data storage/search (#8)
    This PR adds Elasticsearch capabilities and stores all inputs/responses to OpenAI in this ES instance. If a input has already been sent before, the saved response is returned rather than creating a new one.

---
+++ src/ctk_api/microservices/elastic.py
@@ -0,0 +1,159 @@
+""" A client for interacting with Elasticsearch.
+
+This module contains a client for interacting with Elasticsearch. It also
+contains a getter function that is intended to be used as a dependency in
+FastAPI.
+"""
+import logging
+import uuid
+from datetime import datetime
+from typing import Any
+
+import elastic_transport
+import elasticsearch
+import fastapi
+import pydantic
+from fastapi import status
+
+from ctk_api.core import config
+
+settings = config.get_settings()
+ELASTIC_URL = settings.ELASTIC_URL
+ELASTIC_USER = settings.ELASTIC_USER
+ELASTIC_PASSWORD = settings.ELASTIC_PASSWORD
+LOGGER_NAME = settings.LOGGER_NAME
+
+logger = logging.getLogger(LOGGER_NAME)
+
+
+class ElasticClient:
+    """A client for interacting with Elasticsearch.
+
+    Attributes:
+        url: The URL of the Elasticsearch instance.
+        password: The password to use when connecting to the Elasticsearch instance.
+        client: The Elasticsearch client instance.
+    """
+
+    def __init__(
+        self,
+        url: pydantic.AnyHttpUrl | str = ELASTIC_URL,
+        password: pydantic.SecretStr = ELASTIC_PASSWORD,
+    ):
+        logger.info("Initializing Elasticsearch client.")
+        self.url = str(url)
+        logger.debug("Connecting to %s.", self.url)
+        self.client = elasticsearch.Elasticsearch(
+            self.url,
+            basic_auth=(
+                ELASTIC_USER,
+                password.get_secret_value(),  # pylint: disable=no-member
+            ),
+        )
+
+    def create(
+        self, index: str, document: dict[str, Any]
+    ) -> elastic_transport.ObjectApiResponse:
+        """Creates a document in the specified index.
+
+        Args:
+            index: The index to create the document in.
+            body: The document to create.
+
+        Returns:
+            The response from Elasticsearch.
+
+        Notes:
+            The document will be timestamped with the current time.
+            As such, the document should not already have a `created_at` or
+            `modified_at` field.
+        """
+        logger.debug("Creating document in Elasticsearch.")
+        if "created_at" in document or "modified_at" in document:
+            raise fastapi.HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Document should not already have a 'created_at' or 'modified_at' field.",
+            )
+        now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        document["created_at"] = now
+        document["modified_at"] = now
+        document_id = uuid.uuid4().hex
+        return self.client.create(id=document_id, index=index, document=document)
+
+    def read(self, index: str, document_id: str) -> elastic_transport.ObjectApiResponse:
+        """Reads a document from the specified index.
+
+        Args:
+            index: The index to read the document from.
+            document_id: The ID of the document to read.
+
+        Returns:
+            The response from Elasticsearch.
+        """
+        logger.debug("Reading document from Elasticsearch.")
+        return self.client.get(index=index, id=document_id)
+
+    def update(
+        self, index: str, document_id: str, document: dict[str, Any]
+    ) -> elastic_transport.ObjectApiResponse:
+        """Updates a document in the specified index.
+
+        Args:
+            index: The index to update the document in.
+            document_id: The ID of the document to update.
+            body: The document to update.
+
+        Returns:
+            The response from Elasticsearch.
+        """
+        logger.debug("Updating document in Elasticsearch.")
+        if "created_at" in document:
+            raise fastapi.HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Document should not already have a 'created_at' field.",
+            )
+        document["modified_at"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        return self.client.update(index=index, id=document_id, doc=document)
+
+    def delete(self, index: str, document_id: str) -> None:
+        """Deletes a document from the specified index.
+
+        Args:
+            index: The index to delete the document from.
+            document_id: The ID of the document to delete.
+        """
+        logger.debug("Deleting document from Elasticsearch.")
+        self.client.delete(index=index, id=document_id)
+
+    def search(
+        self, index: str, query: dict[str, Any]
+    ) -> elastic_transport.ObjectApiResponse:
+        """Searches the specified index.
+
+        Args:
+            index: The index to search.
+            query: The query to search with.
+
+        Returns:
+            The response from Elasticsearch.
+        """
+        logger.debug("Searching Elasticsearch.")
+        return self.client.search(index=index, query=query)
+
+    def close(self) -> None:
+        """Closes the Elasticsearch client."""
+        logger.debug("Closing Elasticsearch client.")
+        self.client.close()
+
+
+def get_elastic_client() -> ElasticClient:
+    """Creates an instance of the Elasticsearch client.
+
+    Returns:
+        An instance of the Elasticsearch client.
+
+    Notes:
+        This function is used as a dependency in FastAPI.
+    """
+    logger.info("Creating Elasticsearch client.")
+    return ElasticClient()
---
+++ tests/integration/test_elastic.py
@@ -0,0 +1,95 @@
+""" Integration tests for the Elasticsearch client. """
+# mypy: disable-error-code="attr-defined"
+# attr-defined is disabled because elastic_client is not recognized as
+# a TestClass property.
+# pylint: disable=redefined-outer-name
+import pytest
+
+from ctk_api.microservices import elastic
+
+TEST_INDEX = "test_index"
+
+
+@pytest.fixture(scope="function")
+def es_document() -> dict[str, str]:
+    """Returns a test document.
+
+    Notes:
+        This is scoped to functions because something odd is happening
+        when this runs on Github Actions, where it gets modified in place.
+    """
+    return {"test_key": "test_value"}
+
+
+class TestElastic:
+    """This class contains integration tests for the ElasticSearch client.
+    It tests creating, reading, updating, deleting, and searching documents in
+    Elasticsearch.
+    """
+
+    @classmethod
+    def setup_class(cls) -> None:
+        """Set up the test class by initializing an ElasticSearch client."""
+        cls.elastic_client = elastic.get_elastic_client()
+
+    def setup_method(self) -> None:
+        """This method deletes the test index from the Elasticsearch cluster to
+        ensure a clean slate for each test."""
+        self.elastic_client.client.options(ignore_status=[400, 404]).indices.delete(
+            index=TEST_INDEX
+        )
+
+    def test_create(self, es_document: dict[str, str]) -> None:
+        """Tests creating a document in Elasticsearch."""
+        response = self.elastic_client.create(TEST_INDEX, es_document)
+
+        assert response["_index"] == TEST_INDEX
+        assert response["result"] == "created"
+        assert response["_id"]
+
+    def test_search(self, es_document: dict[str, str]) -> None:
+        """Tests searching for documents in Elasticsearch."""
+        self.elastic_client.create(TEST_INDEX, es_document)
+        self.elastic_client.client.indices.refresh()
+
+        response = self.elastic_client.search(TEST_INDEX, {"match_all": {}})
+
+        assert response["hits"]["total"]["value"] == 1
+        assert (
+            response["hits"]["hits"][0]["_source"]["test_key"]
+            == es_document["test_key"]
+        )
+
+    def test_read(self, es_document: dict[str, str]) -> None:
+        """Tests reading a document from Elasticsearch."""
+        document_id = self.elastic_client.create(TEST_INDEX, es_document)["_id"]
+        self.elastic_client.client.indices.refresh()
+
+        response = self.elastic_client.read(TEST_INDEX, document_id)
+
+        assert response["_id"] == document_id
+        assert response["_source"]["test_key"] == es_document["test_key"]
+
+    def test_update(self, es_document: dict[str, str]) -> None:
+        """Tests updating a document in Elasticsearch."""
+        document_id = self.elastic_client.create(TEST_INDEX, es_document)["_id"]
+        self.elastic_client.client.indices.refresh()
+
+        update_response = self.elastic_client.update(
+            TEST_INDEX, document_id, {"test_key": "updated_value"}
+        )
+        read_response = self.elastic_client.read(TEST_INDEX, document_id)
+
+        assert update_response["_id"] == document_id
+        assert read_response["_source"]["test_key"] == "updated_value"
+
+    def test_delete(self, es_document: dict[str, str]) -> None:
+        """Tests deleting a document from Elasticsearch."""
+        document_id = self.elastic_client.create(TEST_INDEX, es_document)["_id"]
+        self.elastic_client.client.indices.refresh()
+
+        self.elastic_client.delete(TEST_INDEX, document_id)
+        self.elastic_client.client.indices.refresh()
+        response = self.elastic_client.search(TEST_INDEX, {"match_all": {}})
+
+        assert response["hits"]["total"]["value"] == 0
